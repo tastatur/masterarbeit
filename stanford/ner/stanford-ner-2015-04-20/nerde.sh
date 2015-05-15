1d83eb24a5ef62527c1c48763701cf177d397217
tastatur <repkov.denis@gmail.com>
2015-05-15 10:52:11 +0200

    Stanford updated

--- stanford/ner/stanford-ner-2015-04-20/nerde.sh
+++ stanford/ner/stanford-ner-2015-04-20/nerde.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -Xmx1g -cp stanford-ner.jar edu.stanford.nlp.process.PTBTokenizer test.txt > test.tok
+java -Xmx1g -cp stanford-ner.jar edu.stanford.nlp.process.PTBTokenizer $1 > test.tok
 perl -ne 'chomp; print "$_ O O O O\n"' test.tok > test.tok.ready
-java -Xmx1g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier classifiers/edu/stanford/nlp/models/ner/german.dewac_175m_600.crf.ser.gz -testFile test.tok.ready > out.dewac.txt
-java -Xmx1g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier classifiers/edu/stanford/nlp/models/ner/german.hgc_175m_600.crf.ser.gz -testFile test.tok.ready > out.hgc.txt
+time java -Xmx5g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier classifiers/edu/stanford/nlp/models/ner/german.dewac_175m_600.crf.ser.gz -testFile test.tok.ready > out.dewac.txt
+time java -Xmx5g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier classifiers/edu/stanford/nlp/models/ner/german.hgc_175m_600.crf.ser.gz -testFile test.tok.ready > out.hgc.txt
