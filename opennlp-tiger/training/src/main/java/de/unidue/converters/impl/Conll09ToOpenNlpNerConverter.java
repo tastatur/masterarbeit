6c55a1e020d806fddcdbaae8342ebd9a23747bb4
tastatur <repkov.denis@gmail.com>
2015-07-05 22:03:59 +0200

    OpenNLP Tiger,  converter und gitignore

--- opennlp-tiger/training/src/main/java/de/unidue/converters/impl/Conll09ToOpenNlpNerConverter.java
+++ opennlp-tiger/training/src/main/java/de/unidue/converters/impl/Conll09ToOpenNlpNerConverter.java
@@ -1,14 +1,20 @@
 package de.unidue.converters.impl;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import de.unidue.converters.Converter;
+import opennlp.tools.util.StringUtil;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Conll09ToOpenNlpNerConverter implements Converter {
+
+    private static final String NE_TAG = "NE";
 
     private final String conll09File;
     private final String openNlpFile;
@@ -19,10 +25,41 @@ public class Conll09ToOpenNlpNerConverter implements Converter {
     }
 
     public void convert() throws IOException {
-        List<String> sentences = new ArrayList<>();
+        final List<String> sentences = new ArrayList<>();
         List<String> lines = Files.lines(new File(conll09File).toPath()).collect(Collectors.toList());
         try (PrintWriter trainingModelWriter = new PrintWriter(new File(openNlpFile))) {
+            Boolean neTagIsNotOpen = true;
+            StringBuilder sentenceBuilder = new StringBuilder();
+            for (String line : lines) {
+                if (StringUtil.isEmpty(line)) {
+                    if (!neTagIsNotOpen) {
+                        sentenceBuilder.append("<END> ").append(" ");
+                        neTagIsNotOpen = true;
+                    }
 
+                    String sentence = sentenceBuilder.toString().replaceAll("''", "").replaceAll("``", "").replaceAll("/", ".");
+                    sentences.add(sentence);
+                    sentenceBuilder = new StringBuilder();
+                } else {
+                    final String dataColumns[] = line.split("\\t");
+                    final String token = dataColumns[1];
+                    final String tag = dataColumns[4];
+
+                    if (!NE_TAG.equals(tag) && neTagIsNotOpen) {
+                        sentenceBuilder.append(token).append(" ");
+                    } else if (NE_TAG.equals(tag) && neTagIsNotOpen) {
+                        neTagIsNotOpen = false;
+                        sentenceBuilder.append("<START:misc> ").append(token).append(" ");
+                    } else if (NE_TAG.equals(tag) && !neTagIsNotOpen) {
+                        sentenceBuilder.append(token).append(" ");
+                    } else if (!NE_TAG.equals(tag) && !neTagIsNotOpen) {
+                        neTagIsNotOpen = true;
+                        sentenceBuilder.append("<END> ").append(token).append(" ");
+                    }
+                }
+            }
+
+            sentences.stream().forEachOrdered(trainingModelWriter::println);
         } catch (IOException e) {
             getLogger().error("Kann die Datei {} nicht erzeugen", openNlpFile);
             throw e;
